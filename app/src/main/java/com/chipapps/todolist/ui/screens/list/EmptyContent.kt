6f5a15a359309ff01bdd5b7352591967f9ce3deb
mjmanaog <mjmanaog16@gmail.com>
2021-11-01 17:08:59 +0800

    feat: list: added empty screen

--- app/src/main/java/com/chipapps/todolist/ui/screens/list/EmptyContent.kt
+++ app/src/main/java/com/chipapps/todolist/ui/screens/list/EmptyContent.kt
@@ -7,13 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import com.chipapps.todolist.R
 import com.chipapps.todolist.ui.theme.EMPTY_ICON
+import com.chipapps.todolist.ui.theme.MediumGray
 
 @Composable
 fun EmptyContent() {
@@ -29,7 +33,20 @@ fun EmptyContent() {
             painter = painterResource(id = R.drawable.ic_empty),
             contentDescription = stringResource(
                 id = R.string.empty_icon
-            )
+            ),
+            tint = MediumGray
+        )
+        Text(
+            text = stringResource(id = R.string.no_task),
+            color = MediumGray,
+            fontWeight = FontWeight.Bold,
+            fontSize = MaterialTheme.typography.h6.fontSize
         )
     }
+}
+
+@Composable
+@Preview
+private fun EmptyContentPreview(){
+    EmptyContent()
 }
