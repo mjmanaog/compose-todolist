ec0a1523705833ff159c8b9177276c34e507d3a6
mjmanaog <mjmanaog16@gmail.com>
2021-11-01 19:45:00 +0800

    feat: created task app bar layout

--- app/src/main/java/com/chipapps/todolist/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/chipapps/todolist/navigation/destinations/TaskComposable.kt
@@ -1,11 +1,13 @@
 package com.chipapps.todolist.navigation.destinations
 
+import android.util.Log
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.chipapps.todolist.util.Action
 import com.chipapps.todolist.util.Constants
+import com.chipapps.todolist.util.Constants.TASK_ARGUMENT_KEY
 
 fun NavGraphBuilder.taskComposable(
     navigateToListScreen: (Action) -> Unit
@@ -15,7 +17,8 @@ fun NavGraphBuilder.taskComposable(
         arguments = listOf(navArgument(Constants.TASK_ARGUMENT_KEY){
             type = NavType.IntType
         })
-    ){
-
+    ){ navBackStackEntry ->
+        val taskId =  navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
+        Log.d("TaskComposable", taskId.toString())
     }
 }
