108678aeb9ce9f5ebb753b2cd0282f27350f9a12
mjmanaog <mjmanaog16@gmail.com>
2021-11-01 14:23:07 +0800

    feat: list: appbar and fab icon layout

--- app/src/main/java/com/chipapps/todolist/navigation/Screens.kt
+++ app/src/main/java/com/chipapps/todolist/navigation/Screens.kt
@@ -5,7 +5,7 @@ import com.chipapps.todolist.util.Action
 import com.chipapps.todolist.util.Constants.LIST_SCREEN
 import com.chipapps.todolist.util.Constants.TASK_SCREEN
 
-class Screens(navController: NavHostController) {
+class Screens (navController: NavHostController) {
     val list: (Action) -> Unit = { action ->
         navController.navigate("list/${action.name}"){
             popUpTo(LIST_SCREEN){
