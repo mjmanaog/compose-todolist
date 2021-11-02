419eb5e5bdae9cd5f18fd90952d95dedc2210f97
mjmanaog <mjmanaog16@gmail.com>
2021-11-02 15:51:27 +0800

    feat: added empty field validations on task screen

--- app/src/main/java/com/chipapps/todolist/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/chipapps/todolist/ui/screens/task/TaskScreen.kt
@@ -1,8 +1,11 @@
 package com.chipapps.todolist.ui.screens.task
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import com.chipapps.todolist.data.models.Priority
 import com.chipapps.todolist.data.models.ToDoTask
 import com.chipapps.todolist.ui.viewmodels.SharedViewModel
@@ -19,9 +22,22 @@ fun TaskScreen(
     val description: String by sharedViewModel.description
     val priority: Priority by sharedViewModel.priority
 
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
-            TaskAppBar(navigateToListScreen = navigateToListScreen, selectedTask = selectedTask)
+            TaskAppBar(navigateToListScreen = { action ->
+                if (action == Action.NO_ACTION || action == Action.DELETE){
+                    navigateToListScreen(action)
+                }else{
+                    if (sharedViewModel.validateFields()){
+                        navigateToListScreen(action)
+                    }else{
+                        displayToast(context = context)
+                    }
+                }
+
+            }, selectedTask = selectedTask)
         },
         content = {
             TaskContent(
@@ -40,4 +56,8 @@ fun TaskScreen(
             )
         }
     )
+}
+
+private fun displayToast(context: Context){
+    Toast.makeText(context, "Field(s) must not be empty", Toast.LENGTH_SHORT).show()
 }
